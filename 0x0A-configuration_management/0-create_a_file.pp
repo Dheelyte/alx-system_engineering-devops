7749509edc077f4166eaa03dda7449730711136f
Delight Olu-Olagbuji <64509158+Dheelyte@users.noreply.github.com>
2023-06-26 10:58:53 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# create a file in /tmp
-
-file { '/tmp/school':
-  owner   => www-data,
-  mode    => '0744',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# install a package
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
